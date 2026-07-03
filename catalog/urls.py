eb677af07a0c7989eee5d5f6fbd09a7aac01fe48
Ann-Fox <fx8565@mail.ru>
2026-07-03 18:12:03 +0500

    catalog: добавлена фильтрация по категории, классовые view

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import catalog_list_view, product_create_view
+from .views import catalog_list_view, product_create_view, CatalogListView
 
 urlpatterns = [
-    path('', catalog_list_view, name='home'),
+    path('', CatalogListView.as_view(), name='home'),
     path('products/create', product_create_view, name='product-create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,51 @@
 from django.shortcuts import redirect, render
 from catalog.forms import ProductForm, ProductImageFormSet
-from catalog.models import Product
+from catalog.models import Category, Product
 from shop.forms_utils import apply_bootstrap_classes
+from django.views.generic import ListView
+
 
 def catalog_list_view(request):
     products = Product.objects.filter(is_active=True)
     # if request.user.is_authentificated and hasattr(request.user, 'customer__profile'):
-        # products = Product.objects.select_related('teacher__user')
-    context = {
-        'products': products
-    }
+    # products = Product.objects.select_related('teacher__user')
+    context = {"products": products}
 
-    return render(request, 'catalog_list.html', context)
+    return render(request, "catalog_list.html", context)
+
+
+class CatalogListView(ListView):
+    model = Product
+    template_name = "catalog_list.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        products = Product.objects.filter(is_active=True)
+        category_slug = self.request.GET.get("category")
+
+        if category_slug:
+            products = products.filter(category__slug=category_slug)
+
+        return products
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.filter(is_active=True)
+        context['selected_category'] = self.request.GET.get('category', '')
+        return context
+
+
+
+
+
 
 
 def product_create_view(request):
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
-        formset = ProductImageFormSet(request.POST, request.FILES, instance=None)  # для нового товара instance=None
+        formset = ProductImageFormSet(
+            request.POST, request.FILES, instance=None
+        )  # для нового товара instance=None
 
         apply_bootstrap_classes(form)
         for image_form in formset:
@@ -27,20 +55,20 @@ def product_create_view(request):
             product = form.save()
             formset.instance = product  # привязываем product к formset
             formset.save()  # сохраняем изображения
-            return redirect('home')  # перенаправляем на каталог
+            return redirect("home")  # перенаправляем на каталог
     else:
-        form = ProductForm(initial={'is_active': True})
+        form = ProductForm(initial={"is_active": True})
         formset = ProductImageFormSet()
 
         apply_bootstrap_classes(form)
         for image_form in formset:
             apply_bootstrap_classes(image_form)
-        
+
     return render(
         request,
-        'product_form.html',
+        "product_form.html",
         {
-            'form': form,
-            'formset': formset,
+            "form": form,
+            "formset": formset,
         },
     )
