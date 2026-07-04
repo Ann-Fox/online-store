1321f69600be8400420934fd4b2bc2a8f0369f60
Ann-Fox <fx8565@mail.ru>
2026-07-04 21:47:21 +0500

    vue: настройки vue, catalog - vue

--- shop-vue/src/views/HomeView.vue
+++ shop-vue/src/views/HomeView.vue
@@ -1,9 +1,64 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import {onMounted, ref} from 'vue'
+import axios from 'axios'
+
+const productList = ref([])
+
+const getProductList = async()=>{
+  let res = await axios.get('/products/product-json/')
+  productList.value = res.data
+}
+
+onMounted(()=>{
+  getProductList()
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <!-- Фильтр по категориям -->
+    <div class="row mb-4">
+      <div class="col-md-12">
+        <form method="get" class="row g-3">
+          <div class="col-auto">
+            <label for="categoryFilter" class="visually-hidden">Категория</label>
+            <select name="category" id="categoryFilter" class="form-select" onchange="this.form.submit()">
+              <option value="">Все категории</option>
+              <option value="">
+              </option>
+            </select>
+          </div>
+          <div class="col-auto">
+            <button type="submit" class="btn btn-outline-success">
+              Применить
+            </button>
+            <a href="" class="btn btn-secondary">Сбросить</a>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="row gy-5">
+      <div class="col-4" v-for="product in productList">
+        <div class="card" style="width: 18rem">
+           <img
+           v-if="product.image_url"
+          :src="product.image_url"
+          class="card-img-top"
+          :alt="product.name"
+          style="height: 200px; object-fit: cover"
+        />
+          <div v-else class="card-img-top bg-light d-flex align-items-center justify-content-center"
+            style="height: 200px; color: #6c757d">
+            Нет фото
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p class="card-text">{{ product.description }}</p>
+            <p class="card-text"><strong>{{ product.price }} ₽</strong></p>
+            <a href="" class="btn btn-success">К товару</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
